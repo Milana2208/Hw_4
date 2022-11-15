9e69572cb0d5490f28d3fb07a1167aa6d902c17a
Milana2208 <117971563+Milana2208@users.noreply.github.com>
2022-11-15 16:40:16 +0600

    'dz'

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
-const block = document.getElementById("block");
-const x = document.getElementById("x")
-const y = document.getElementById("y")
-function showCoordination(){
-    x.innerText = event.clientX;
-    y.innerText = event.clienty;
-}
-block.addEventListener("mousemove", showCoordination)
+// const block = document.getElementById("block");
+// const x = document.getElementById("x")
+// const y = document.getElementById("y")
+// function showCoordination(){
+//     x.innerText = event.clientX;
+//     y.innerText = event.clienty;
+// }
+// block.addEventListener("mousemove", showCoordination)
+//
+
+// const mainus = document.getElementById('mainus')
+// const plus = document.getElementById('plus')
+// const number = document.getElementById('number')
+// let o = 0;
+// mainus.addEventListener("click", function (){
+//     if (o > 0){
+//         o--;
+//         number.style.color = 'red';
+//         number.innerHTML = o
+//
+//     }
+// })
+// plus.addEventListener('click', function (){
+//     o++;
+//     number.style.color = "green";
+//     number.innerHTML = o
+// })
+
+
+const y = document.getElementById('y_y')
+const x = document.getElementById('x_x')
+const box = document.getElementById("box")
+let yy;
+let xx;
+box.addEventListener('mousemove', e => {
+    yy = e.screenY
+    xx = e.screenX
+    y.innerHTML = yy
+    x.innerHTML = xx
+})
